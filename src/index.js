a37b993884020ddd844752452d976edadf5d3393
Disa0808 <levshukdenis@mail.ru>
2018-09-16 23:58:16 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsStore = {};
+
+  // Fill braces object
+  // key - close bracket
+  // value - open bracket
+  for(let i = 0; i < bracketsConfig.length; i++) {
+  bracketsStore[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  
+  // Stack for storing opened brackets
+  var stack = [];
+  
+  // All magic inside
+  for(var i = 0; i < str.length; i++) {
+  // Get current bracket
+  var char = str.charAt(i);
+  
+  // Check if it's open bracket
+  // or if it | and last stack element is not |
+  if (!bracketsStore[char] || bracketsStore[char] && char == bracketsStore[char] && stack[stack.length - 1] != char) {
+  // And push open bracket into stack
+  stack.push(char);
+  } else {
+  // Otherwise check if stack is empty
+  // or last value from stack not equal to open bracket for current char
+  if(stack.length == 0 || stack.pop() != bracketsStore[char]) {
+  return false;
+  }
+  }
+  }
+  // If stack is empty then return true, otherwise - false;
+  return stack.length == 0 ? true : false;
 }
